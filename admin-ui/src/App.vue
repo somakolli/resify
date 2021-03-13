dc749883978fbc0411925b816d2faa805cfdf387
Sokol Makolli <st116214@stud.uni-stuttgart.de>
2021-03-13 12:46:15 +0100

    add under construction to admin-ui

--- admin-ui/src/App.vue
+++ admin-ui/src/App.vue
@@ -1,3 +1,3 @@
 <template>
-  <router-view />
+  <div class="w-full h-screen flex justify-center content-center align-middle"><div class="self-center">Site under construction.</div></div>
 </template>
