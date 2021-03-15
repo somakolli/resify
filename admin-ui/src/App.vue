c3bd03a45edf5aa9c456f8ad6248bb7cb85e1f5e
Sokol Makolli <st116214@stud.uni-stuttgart.de>
2021-03-15 14:18:28 +0100

    reformating

--- admin-ui/src/App.vue
+++ admin-ui/src/App.vue
@@ -1,3 +1,5 @@
 <template>
-  <div class="w-full h-screen flex justify-center content-center align-middle"><div class="self-center">Site under construction.</div></div>
+  <div class="w-full h-screen flex justify-center content-center align-middle">
+    <div class="self-center">Site under construction.</div>
+  </div>
 </template>
