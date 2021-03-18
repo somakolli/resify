782295061531e890c6493b36ac77e352098610c3
Sokol Makolli <st116214@stud.uni-stuttgart.de>
2021-03-18 20:15:32 +0100

    implement services

---
+++ customer-ui/components/ListItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <div
+    @click="$emit('click')"
+    class="flex w-full bg-white h-10 shadow rounded-full justify-between items-center pl-5"
+  >
+    <div class="overflow-auto font-semibold">{{ mainContent }}</div>
+    <div
+      class="flex justify-center items-center bg-white shadow rounded-full min-w-16 px-2 h-full"
+    >
+      <span class="mr-1 font-semibold">{{ duration }}m</span>
+      <div
+        class="shadow rounded-full h-full w-10 -mr-2 flex items-center justify-center"
+      >
+        <Icon class="w-8 h-8 text-green-700" :icon-name="iconName"></Icon>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Icon from "./shared-components/Icon.vue";
+import { computed } from "@nuxtjs/composition-api";
+export default {
+  components: { Icon },
+  emits: ["click"],
+  props: {
+    sideContent: {
+      type: String,
+      required: false,
+    },
+    mainContent: {
+      type: String,
+      required: true,
+    },
+    iconName: {
+      type: String,
+      required: false,
+    },
+    showIcon: {
+      type: Boolean,
+      required: false,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+  },
+  setup(props: any) {
+    const mainContentTrimmed = computed(() => {
+      if (props.mainContent.length > 16) {
+        return props.mainContent.slice(0, 14) + " ...";
+      } else {
+        return props.mainContent;
+      }
+    });
+    return {
+      mainContentTrimmed,
+    };
+  },
+};
+</script>
--- share/components/IconRepository.ts
+++ share/components/IconRepository.ts
@@ -19,6 +19,7 @@ export type IconName =
   | "adjustments"
   | "exclamation-circle"
   | "arrow-down"
+  | "x"
   | "arrow-up";
 
 class IconRepository {
@@ -118,6 +119,10 @@ class IconRepository {
       "arrow-up",
       `<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7"></path></svg>`,
     ],
+      [
+        "x",
+        `<svg class="" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>`
+      ],
   ]);
 }
 
